c6e49adad450e749361b6fa627145cdb10760044
Johana Infesta <johana.infesta@gmail.com>
2018-11-26 11:51:09 -0300

    fin clase 3

--- app/routes/personaje.js
+++ app/routes/personaje.js
@@ -1,8 +1,14 @@
 import Route from '@ember/routing/route';
 
-export default Route.extend({
-  Model(params){
+import { inject as service } from '@ember/service';
 
-    let Url = ''
-}
+export default Route.extend({
+  ajax: service(),
+
+  model(params){
+
+    let requestUrl = 'https://gateway.marvel.com:443/v1/public/characters/' +
+    params.personaje_id + '?apikey=6b4e24ceb63521fdaf4ae2b29c5accb6';
+    return this.get('ajax').request(requestUrl);
+  }
 });
