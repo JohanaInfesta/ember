536ea5913bb4ffc9d3b8ad869c68aa5dfc0f58e2
Johana Infesta <johana.infesta@gmail.com>
2018-11-26 10:59:26 -0300

    clase 3 ember

---
+++ app/models/personajes.js
@@ -0,0 +1,11 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+
+  name: DS.attr(),
+  description: DS.attr(),
+  // age: DS.attr('number'),
+  image: computed('path','extension', function(){
+    return path + '.' + extension;
+  });
+});
--- app/routes/search-results.js
+++ app/routes/search-results.js
@@ -1,16 +1,25 @@
 import Route from '@ember/routing/route';
 
+  import { inject as service } from '@ember/service';
+
 export default Route.extend({
+  ajax: service(),
+
   model(params, transition){
   console.log(transition.queryParams['name']);
-    return[{
-      name:'Thor',
-      image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTD01BaaGXp_Bs5zAvPm8Xu_8a1wzsT1cP2b-RRmTNARjLMFS9D',
-      description:'anda?'},
-      {
-        name:'Capitan America',
-        image:'https://images-na.ssl-images-amazon.com/images/I/71HIeG-PkzL._SY679_.jpg',
-        description:'anda?'
-      }]
+
+let requestUrl = 'https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=' +
+ transition.queryParams['name'] + '&apikey=6b4e24ceb63521fdaf4ae2b29c5accb6';
+ return this.get('ajax').request(requestUrl);
+
+    // return[{
+    //   name:'Thor',
+    //   image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTD01BaaGXp_Bs5zAvPm8Xu_8a1wzsT1cP2b-RRmTNARjLMFS9D',
+    //   description:'anda?'},
+    //   {
+    //     name:'Capitan America',
+    //     image:'https://images-na.ssl-images-amazon.com/images/I/71HIeG-PkzL._SY679_.jpg',
+    //     description:'anda?'
+    //   }]
     }
   });
